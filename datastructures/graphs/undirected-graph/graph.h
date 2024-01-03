079a825b998e0eebc453c5fcd12714a96bbbcea8
raphsenn <raphaelsenn@outlook.de>
2024-01-03 17:01:09 +0100

    graph

---
+++ datastructures/graphs/undirected-graph/graph.h
@@ -0,0 +1,29 @@
+#include <map>
+#include <any>
+
+// Easy implementation of an undirected graph.
+// In an undirected graph, edges have no direction, and they
+// represent symmetric relationships between nodes.
+//
+// In one restricted but very common sense of the term,a graph is an ordered pair G = ( V , E ) G=(V,E) comprising:
+// V, a set of vertices (also called nodes or points),
+// E  a set of edges (also called links or lines), which are unordered pairs of vertices
+// (that is, an edge is associated with two distinct vertices).
+
+class Graph {
+
+  private:
+    std::vector<std::any> vertices;
+    std::vector<std::any> edges;
+    std::map<std::any, std::vector<std::any>> adjacency_list;
+
+  public:
+    // V set of verteces
+    // E set of edges
+    Graph(std::vector<any> V, std::vector<std::tuple<std::any, std::any>> E);
+    void create_graph();
+    std::map<std::any, std::vector<std::any>> get_adjacency_list;
+
+};
+
+
---
+++ datastructures/inverted-index-with-BM25-and-evaluation/include/evaluate.h
@@ -0,0 +1,15 @@
+#include "./inverted_index.h"
+
+class Evaluate {
+
+  private:
+
+
+  public:
+    Evaluate();
+    void read_from_benchmark(std::string file_name);
+
+
+
+};
+
