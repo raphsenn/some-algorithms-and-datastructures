7067928daf0e7d84618e14029ec2e55d12592bfc
raphsenn <raphaelsenn@outlook.de>
2024-01-07 07:42:05 +0100

    create directed graph

---
+++ datastructures/graphs/directed-graph-with-weights/graph.cpp
@@ -0,0 +1,77 @@
+#include "./graph.h"
+
+Graph::Graph(std::vector<int> V, std::vector<std::tuple<int, int>> E)
+  : vertices(V), edges(E) { check_if_edges_missing(); create_adjacency_list(); }
+
+
+void Graph::check_if_edges_missing() {
+  for (int i = 0; i < edges.size(); i++) {
+    std::tuple edge= std::make_tuple(std::get<1>(edges[i]), std::get<0>(edges[i]));
+    if (!edge_in_edges(edge)) { 
+      edges.emplace_back(edge);
+    }
+  }
+}
+
+bool Graph::edge_in_edges(std::tuple<int, int> edge) {
+  if (std::find(edges.begin(), edges.end(), edge) != edges.end()) {
+    return true; 
+  }
+  return false;
+}
+
+void Graph::create_adjacency_list() {
+  for (int i = 0; i < vertices.size(); i++) {
+    adjacency_list[i] = {};
+  }
+  
+  for (int i = 0; i < edges.size(); i++) {
+    int from_vertex = std::get<0>(edges[i]);
+    int to_vertex = std::get<1>(edges[i]);
+    adjacency_list[from_vertex].emplace_back(to_vertex);
+    }
+  }
+
+void Graph::create_adjacency_matrix() {
+  for (int i = 0; i < vertices.size(); i++) {
+    for (int j = 0; j < vertices.size(); j++) {
+      if (edge_in_edges(std::make_tuple(i, j))) {
+        adjacency_matrix[i][j] = 1;
+      } else { adjacency_matrix[i][j] = 0; }; 
+    }
+  }
+}
+
+std::unordered_map<int, std::vector<int>> get_adjacency_list() { return adjacency_list; }
+
+std::vector<int> Graph::get_vertices(){ return vertices; }
+
+std::vector<std::tuple<int, int>> Graph::get_edges(){ return edges; }
+
+int Graph::in_degree(int vertex) {
+  int degree = 0; 
+  for (int i = 0; i < edges.size(); i++) {
+    if (std::get<1>(edges[i]) == vertex) {degree++;}
+  }
+  return degree;
+}
+
+int Graph::out_degree(int vertex) { return adjacency_list[vertex].size(); }
+
+void Graph::print() {
+  printf("{"); 
+  for (auto it = adjacency_list.begin(); it != adjacency_list.end(); ++it) {
+    printf("%d: [", it->first);
+    for (int j = 0; j < it->second.size(); j++) {
+      if (j != it->second.size()-1) {
+        printf("%d, ", it->second[j]);
+      } else { printf("%d", it->second[j]); }
+    }
+    printf("]");
+    if (std::next(it) != adjacency_list.end()) {
+      printf(", ");
+    }
+  }
+  printf("}\n");
+  return;
+}
---
+++ datastructures/graphs/directed-graph-with-weights/graph.h
@@ -0,0 +1,71 @@
+//////////////////////////////////////////////////////////////////////////////
+// Easy implementation of an undirected graph.
+// ///////////////////////////////////////////////////////////////////////////
+//
+// In an undirected graph, edges have no direction, and they
+// represent symmetric relationships between nodes.
+// For example, when A goes to B, so B must go to A.
+//
+// A graph is an ordered pair G = ( V , E ).
+// V is a set of vertices (also called nodes or points),
+// and E is a set of edges (also called links or lines), which are unordered
+// pairs of vertices.
+//
+// We are using a adjacency list for the graph representation, but
+// a adjacency matrix is also included.
+//////////////////////////////////////////////////////////////////////////////
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <any>
+#include <tuple>
+#include <cstdio>
+#include <string>
+#include <algorithm>
+
+class Graph {
+  // Let n be the number of verteces and m the number of edges from
+  // the graph.
+  private:
+    std::vector<int> vertices;
+    std::vector<std::tuple<int, int>> edges;
+    std::unordered_map<int, std::vector<int>> adjacency_list;
+    std::vector<std::vector<int>> adjacency_matrix;
+
+  public:
+    // V vector<any> of verteces, E vector<any> of edges.
+    Graph(std::vector<int> V, std::vector<std::tuple<int, int>> E);
+
+    // Fill missing edges. 
+    void check_if_edges_missing(); 
+
+    // Returns true if edge in edges, else false.
+    bool edge_in_edges(std::tuple<int, int> edge);
+
+    // Fill the adjacency list.
+    void create_adjacency_list();
+
+    // Fill the adjacency matrix.
+    void create_adjacency_matrix();
+
+    // Returns vector of Vertices.
+    std::vector<int> get_vertices();
+   
+    // Returns vector of tuple of edges.
+    std::vector<std::tuple<int, int>> get_edges();
+
+    // Returns the in-degree of an vertex.
+    int in_degree(int vertex);
+
+    // Returns the out-degree of an vertex.
+    int out_degree(int vertex);
+
+    // Returns the adjacency list.
+    std::unordered_map<int, std::vector<int>> get_adjacency_list();
+   
+    // Returns the adjacency matrix.
+    std::vector<std::vector<int>> get_adjacency_matrix();
+  
+    // Print graph as adjacency list.
+    void print();
+};
